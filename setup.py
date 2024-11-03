3b1d5ddb17038476842a7652df250a690ce1983e
Ayush bhujade <ayushbhujade2005@gmail.com>
2024-11-03 11:07:51 +0530

    setup is done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="Generative AI Project",
+    version='0.0.0.0',
+    author="Ayush Bhujade",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
